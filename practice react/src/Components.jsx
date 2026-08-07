88bdb10e275ad274423d7da8013ca6eceaa2c597
Hoorain Eman <24pwbcs1389@uetpeshawar.edu.pk>
2026-08-07 16:00:12 +0500

    about components

---
+++ practice react/src/Components.jsx
@@ -0,0 +1,17 @@
+function Component(){
+    return(
+        <>
+        <h1>Component</h1>
+        </>
+    )
+}
+
+export default Component //only one can be default
+export function NextComp(){
+    return(
+        <>
+        <h1>Second compoent </h1>
+        </>
+    )
+}
+//multiple componets will be naming componets not default
---
+++ practice react/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Components , {NextComp} from './Components.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Components />
+    <NextComp />
+  </StrictMode>,
+)
